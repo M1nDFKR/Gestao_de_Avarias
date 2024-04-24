3c3134235709853795a4f815bcd8033696afb53c
Patrick0x0 <patrick.bichiarov@hotmail.com>
2024-04-24 09:39:48 +0100

    Ligação a base de dados "OK"

--- Pap/Form1.cs
+++ Pap/Form1.cs
@@ -1,3 +1,5 @@
+using MySql.Data.MySqlClient;
+
 namespace Pap
 {
     public partial class Form1 : Form
@@ -9,12 +11,23 @@ namespace Pap
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            try
+            {
+                var strConexao = "server=localhost;uid=root;pwd=292822;database=gestao_de_avaria";
+                var conexao = new MySqlConnection(strConexao);
+                conexao.Open();
+                MessageBox.Show("Conexão OK");
 
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ocorreu um erro ao conectar a DB: " + ex.Message);
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show($"Hello {textBox1.Text} {textBox2.Text}");
+            //MessageBox.Show($"Hello {textBox1.Text} {textBox2.Text}");
         }
     }
 }
